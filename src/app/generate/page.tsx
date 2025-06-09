904d28b1b1a830152413e0c127905e8267954580
emilija <emilija1705@gmail.com>
2025-06-10 00:18:52 +0200

    make landing page nicer

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -4,12 +4,12 @@ import RopaForm from "@/components/ropa-form";
 import RopaPreview from "@/components/ropa-preview";
 import {useState} from "react";
 
-export default function Home() {
+export default function Generate() {
 
     const [generatedDocument, setGeneratedDocument] = useState<string>("");
 
     return (
-        <main className="container mx-auto py-10 px-4 md:px-6 relative space-y-6">
+        <main className="container mx-auto py-10 relative space-y-6 w-full">
             <RopaForm setGeneratedDocument={setGeneratedDocument}/>
             {generatedDocument != "" && <RopaPreview generatedDocument={generatedDocument} setGeneratedDocument={setGeneratedDocument}/>}
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,19 @@ import {TypingAnimation} from "@/components/magicui/typing-animation";
 import BlurText from "@/components/ui/blur-text";
 import CircularText from "@/components/ui/circular-text";
 import MovingBanner from "@/components/ui/moving-banner";
+import SpotlightCard from '@/components/ui/spotlight-card';
+import {CardHeader, CardContent} from "@/components/ui/card";
+import { BrainCircuit } from 'lucide-react';
+import { Landmark } from 'lucide-react';
+import { UserCheck } from 'lucide-react';
 
 
 export default function Home() {
     return (
-        <div>
+        <>
             {/* Hero Section */}
             <section
-                className="h-[calc(100vh-4rem)] overflow-hidden md:flex md:flex-col md:justify-center md:items-start md:pb-10"
+                className="h-[calc(100vh-6rem)] container overflow-hidden md:flex md:flex-col md:justify-center md:items-start md:pb-10"
             >
 
                 <div className="fixed right-8 bottom-8 z-50">
@@ -52,11 +57,42 @@ export default function Home() {
                 </div>
             </section>
 
-            <section className="overflow-hidden">
-                <MovingBanner/>
-            </section>
+            <div className="-mx-2 md:-mx-4 w-screen ">
+                <MovingBanner />
+            </div>
+
+            <section className="py-10 grid grid-cols-1 md:grid-cols-3 gap-6">
+    <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(102, 102, 255, 0.2)">
+        <CardHeader className="p-6 pb-0 flex items-center">
+            <BrainCircuit className="mr-2 size-14" />
+            <h1 className="text-2xl">Powered by AI</h1>
+        </CardHeader>
+        <CardContent className="p-6">
+            ROPAgen uses advanced AI algorithms to generate your ROPA files quickly and accurately.
+        </CardContent>
+    </SpotlightCard>
+    <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(102, 102, 255, 0.2)">
+        <CardHeader className="p-6 pb-0 flex items-center">
+            <Landmark className="mr-2 size-14" />
+            <h1 className="text-2xl">GDPR-compliant</h1>
+        </CardHeader>
+        <CardContent className="p-6">
+            ROPAgen ensures that your ROPA files meet all GDPR requirements, so you can focus on your business.
+        </CardContent>
+    </SpotlightCard>
+    <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(102, 102, 255, 0.2)">
+        <CardHeader className="p-6 pb-0 flex items-center">
+            <UserCheck className="mr-2 size-14" />
+            <h1 className="text-2xl">Easy to use</h1>
+        </CardHeader>
+        <CardContent className="p-6">
+            With a user-friendly interface, ROPAgen makes it simple to create and manage your ROPA files. It even offers templates to get you started quickly.
+        </CardContent>
+    </SpotlightCard>
+</section>
+
 
             {/* …rest of your normal page sections… */}
-        </div>
+        </>
     )
 }
